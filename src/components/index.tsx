f1cacc36430fa6cc5fd96c114c01673d4aa1b558
afeiship <1290657123@qq.com>
2024-04-26 22:45:27 +0800

    feat: add backdrop

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -34,41 +34,54 @@ export default class ReactDialog extends Component<ReactDialogProps> {
     visible: false,
     fixed: false,
     withBackdrop: false,
-    keepMounted: false,
+    keepMounted: false
   };
 
   private dialogRef = React.createRef<HTMLDialogElement>();
+  private backdropRef = React.createRef<HTMLDivElement>();
 
+  // ---- dom elements ----
   get dialog() {
     return this.dialogRef.current as HTMLDialogElement;
   }
 
+  get backdrop() {
+    return this.backdropRef.current as HTMLDivElement;
+  }
+
+  // ---- computed props ----
   get isVisible() {
     return this.dialog.open;
   }
 
+  // ---- state react ----
   state = {
-    stateVisible: this.props.visible,
+    stateVisible: this.props.visible
   };
 
+  // ---- life cycle start ----
   componentDidMount() {
     const { visible } = this.props;
     if (visible) this.present();
   }
 
-  shouldComponentUpdate(nextProps): boolean {
+  shouldComponentUpdate(nextProps: ReactDialogProps): boolean {
     const { visible } = nextProps;
     if (visible) this.present();
     if (!visible) this.dismiss();
     return true;
   }
 
+  // ---- life cycle end ----
+
+
+  // ---- public methods ----
   present = () => {
     if (this.isVisible) return;
-    const { withBackdrop } = this.props;
-    const showMethod = withBackdrop ? 'showModal' : 'show';
-    this.dialog[showMethod]();
+    // const { withBackdrop } = this.props;
+    this.dialog.show();
     this.setState({ stateVisible: this.isVisible });
+    this.backdrop.removeAttribute('hidden');
   };
 
   dismiss = () => {
@@ -76,14 +89,20 @@ export default class ReactDialog extends Component<ReactDialogProps> {
     const el = this.dialog;
     el.classList.add('is-hide');
     el.addEventListener('webkitAnimationEnd', this.handleAnimationEnd, false);
+    this.backdrop.addEventListener('webkitAnimationEnd', this.handleBackdropAnimationEnd, false);
   };
 
-  handleAnimationEnd = () => {
+  private handleAnimationEnd = () => {
     const el = this.dialog;
     el.classList.remove('is-hide');
     el.close();
     el.removeEventListener('webkitAnimationEnd', this.handleAnimationEnd, false);
     this.setState({ stateVisible: this.isVisible });
+  };
+
+  private handleBackdropAnimationEnd = () => {
+    if (this.state.stateVisible) return;
+    this.backdrop.hidden = true;
   };
 
   render() {
@@ -92,15 +111,22 @@ export default class ReactDialog extends Component<ReactDialogProps> {
     const keepChildren = keepMounted || stateVisible;
 
     return (
-      <dialog
-        data-component={CLASS_NAME}
-        data-backdrop={withBackdrop}
-        data-fixed={fixed}
-        className={classNames(CLASS_NAME, className)}
-        ref={this.dialogRef}
-        {...props}>
-        {keepChildren ? children : null}
-      </dialog>
+      <>
+        <dialog
+          data-component={CLASS_NAME}
+          data-fixed={fixed}
+          className={classNames(CLASS_NAME, className)}
+          ref={this.dialogRef}
+          {...props}>
+          {keepChildren ? children : null}
+        </dialog>
+        <div
+          hidden
+          data-visible={withBackdrop && stateVisible}
+          className={`${CLASS_NAME}__backdrop`}
+          ref={this.backdropRef}
+        />
+      </>
     );
   }
 }
